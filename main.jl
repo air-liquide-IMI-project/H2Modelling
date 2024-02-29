bbeacf33758caf990424482e3ad885c90a6ce670
Nicolas-Bessin <nicolasbessin49@gmail.com>
2024-02-29 08:33:13 +0100

    update to use one generic solver

---
+++ main.jl
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Julia 1.9.4
+#     language: julia
+#     name: julia-1.9
+# ---
+
+# ## Zeroth Step
+# ### Importing Libraries and Loading Data
+
+# +
+GC.gc() # Clear up the memory when doing a "run all" in the notebook
+
+using Plots
+gr()
+
+include("src/load_profiles.jl")
+include("src/solver.jl");
+# -
+
+filename = "profiles.csv"
+time_profiles, wind_profiles, solar_profiles = Dict(), Dict(), Dict()
+time_profiles, wind_profiles, solar_profiles = load_profiles(filename);
+
+# ### We first define the global parameters of the problem, to have consistent results across solving methods
+
+# Default values
+DEMAND = 1000. # Kg of H2
+# Battery parameters
+EBAT = 0.9 # per month discharge
+FBAT = 100. # MW
+COST_BAT = 250000 * 0.0002 # € / MWh
+# Electrolyzer parameters
+EELEC = 0.050 # Mwh / Kg
+COST_ELEC =  1200000 * 0.0004 # € / MW
+CAPA_ELEC_UPPER = 1000 # MW
+# Tank parameters
+COST_TANK = 407. # € / Kg
+# Grid parameters
+PRICE_GRID = 1000. # € / MWh
+PRICE_CURTAILING = 1000. # € / MWh;
+PRICE_PENALITY = 10. # € / times changed
+# Renewable pricing, from https://atb.nrel.gov/electricity/2022/index
+# For now : 20 year lifespan, no discount rate + O&M cost per year
+COST_WIND = 1352 * 1000 / 20 + 43 * 1000 # € / MW
+COST_SOLAR = 1233 * 1000 / 20 + 23 * 1000 # € / MW
+# Upper bound on the battery capacity
+CAPA_BAT_UPPER = 40000 # MW
+#Year chosen for the simulation
+YEAR = 2015;
+
+# +
+function print_solution_propreties(
+    output::Dict{String, Any}
+)
+    # Extract the solution values
+    battery_capa = trunc(Int64, output["battery_capa"])
+    tank_capa = trunc(Int64, output["tank_capa"])
+    electro_capa = trunc(Int64, output["electro_capa"])
+    wind_capa = trunc(Int64, output["wind_capa"])
+    solar_capa = trunc(Int64, output["solar_capa"])
+    storage_cost = output["storage_cost"]
+    operating_cost = output["operating_cost"]
+    electrolyser_cost = output["electrolyser_cost"]
+    electricity_cost = output["electricity_plant_cost"]
+    prod_out = output["prod"]
+    charge_out = output["charge"]
+    stock_out = output["stock"]
+    elec_out = output["elecGrid"]
+    curtailment_out = output["curtail"]
+    consPPA_out = output["elecPPA"];
+    # Capacities
+    println("Battery capacity: $battery_capa, Tank capacity: $tank_capa kg, Electrolyser capacity: $(electro_capa / EELEC) kg/h")
+    println("Wind capacity: $wind_capa MW, Solar capacity: $solar_capa MW \n")
+    # Energy Mix 
+    mix_in_capacity = wind_capa / (wind_capa + solar_capa)
+    mix_in_generation = wind_capa * sum(wind_profile) / (wind_capa * sum(wind_profile) + solar_capa * sum(solar_profile))
+    println("Wind proportion in capacity: $mix_in_capacity \nWind proportion in generation: $mix_in_generation \n")
+    # Costs
+    println("Storage cost: $storage_cost, operating cost: $operating_cost")
+    println("Electrolyser cost : $electrolyser_cost, electricity plant cost: $electricity_cost")
+    println("Total cost: $(storage_cost + operating_cost + electrolyser_cost + electricity_cost)")
+
+end;
+# -
+
+function plot_solution(
+    output::Dict
+)
+    # Extract the solution values
+    battery_capa = trunc(Int64, output["battery_capa"])
+    tank_capa = trunc(Int64, output["tank_capa"])
+    electro_capa = trunc(Int64, output["electro_capa"])
+    wind_capa = trunc(Int64, output["wind_capa"])
+    solar_capa = trunc(Int64, output["solar_capa"])
+    storage_cost = output["storage_cost"]
+    operating_cost = output["operating_cost"]
+    electrolyser_cost = output["electrolyser_cost"]
+    electricity_cost = output["electricity_plant_cost"]
+    prod_out = output["prod"]
+    charge_out = output["charge"]
+    stock_out = output["stock"]
+    elec_out = output["elecGrid"]
+    curtailment_out = output["curtail"]
+    consPPA_out = output["elecPPA"];
+    # Plot the production & tank charge over time
+    prod = plot(size=(1200, 500), legend=:topleft, xlabel="Time (h)", ylabel="Production (Kg)",
+    title="Hydrogen, Demand : $D kg/h, Tank, Electrolyser: $(electro_capa / EELEC) kg/h")
+    plot!(prod, prod_out, label="Production")
+    # Plot the consumptions, curtailment and battery charge
+    cons = plot(size=(1200, 500), legend=:topleft, xlabel="Time (h)", ylabel="Electricity consumption (Mwh)",
+    title="Consumption, Solar capacity: $(trunc(solar_capa)) MW, Wind capacity: $(trunc(wind_capa)) MW")
+    plot!(cons, elec_out, label="Grid consumption")
+    plot!(cons, consPPA_out, label="PPA consumption")
+    plot!(cons, -curtailment_out, label="Curtailment")
+    # Plot the charge levels
+    level_bat = plot(size=(1200, 500), legend=:topleft, xlabel="Time (h)", ylabel="Battery charge (Mwh)",
+     title="Battery charge level, Battery capacity : $battery_capa MWh")
+    plot!(level_bat, charge_out, label="Battery charge")
+    level_tank = plot(size=(1200, 500), legend=:topleft, xlabel="Time (h)", ylabel="Tank charge (Kg)",
+     title="Tank charge level, Tank capacity : $tank_capa Kg")
+    plot!(level_tank, stock_out, label="Tank charge")
+
+    return prod, cons, level_bat, level_tank
+end;
+
+# # First Step :
+# ## Solve the problem given the production and storage capacities
+
+# We use the default values for now
+BAT_SIZE = 400. # MWh
+TANK_SIZE = 500. # Kg
+ELEC_CAPA = 1000. # MW
+time_index = wind_profiles[YEAR]
+wind_profile = wind_profiles[YEAR]
+solar_profile = solar_profiles[YEAR]
+# We decide on a mix of wind and solar
+# For example, 25% of the energy comes from solar and 75% from wind
+wind_share = 0.75
+Total_energy_needed = D * length(wind_profile) * EELEC * 1.05 # in MWh, with a 5% safety margin
+wind_capa = Total_energy_needed * wind_share / sum(wind_profile)
+solar_capa = Total_energy_needed * (1 - wind_share) / sum(solar_profile);
+
+output1 = solve(
+    wind_profile, solar_profile, # WIND_PROFILE, SOLAR_PROFILE
+    DEMAND, # DEMAND
+    wind_capa, solar_capa, # WIND_CAPA, SOLAR_CAPA, if set to missing, the solver will optimize it
+    BAT_SIZE, TANK_SIZE, ELEC_CAPA, # BAT_SIZE, TANK_SIZE, ELEC_CAPA, if set to missing, the solver will optimize it
+    PRICE_GRID, PRICE_CURTAILING, PRICE_PENALITY, # PRICE_GRID, PRICE_CURTAILING, PRICE_PENALITY
+    CAPA_BAT_UPPER, CAPA_ELEC_UPPER, # CAPA_BAT_UPPER, CAPA_ELEC_UPPER
+    EBAT, FBAT, EELEC, # EBAT, FBAT, EELEC
+    COST_ELEC, COST_BAT, COST_TANK, # COST_ELEC, COST_BAT, COST_TANK
+    COST_WIND, COST_SOLAR, # COST_WIND, COST_SOLAR
+    0., # No initial charge
+    0., # No initial stock
+    missing, # No final charge constraint
+    missing, # No final stock constraint
+);
+
+print_solution_propreties(output1)
+
+prod1, cons1, bat1, tank1 = plot_solution(output1);
+
+prod1
+
+cons1
+
+bat1
+
+tank1
+
+# # Second Step :
+# ## Solve the problem given the production capacities only
+
+# We use the default values for now
+D = 1000.
+time_index = wind_profiles[YEAR]
+wind_profile = wind_profiles[YEAR]
+solar_profile = solar_profiles[YEAR]
+# We decide on a mix of wind and solar
+# For example, 25% of the energy comes from solar and 75% from wind
+wind_share = 0.75
+Total_energy_needed = D * length(wind_profile) * EELEC * 1.05 # in MWh, with a 5% safety margin
+wind_capa = Total_energy_needed * wind_share / sum(wind_profile)
+solar_capa = Total_energy_needed * (1 - wind_share) / sum(solar_profile);
+
+output2 = solve(
+    wind_profile, solar_profile, # WIND_PROFILE, SOLAR_PROFILE
+    DEMAND, # DEMAND
+    wind_capa, solar_capa, # WIND_CAPA, SOLAR_CAPA, if set to missing, the solver will optimize it
+    missing, missing, missing, # BAT_SIZE, TANK_SIZE, ELEC_CAPA, if set to missing, the solver will optimize it
+    PRICE_GRID, PRICE_CURTAILING, PRICE_PENALITY, # PRICE_GRID, PRICE_CURTAILING, PRICE_PENALITY
+    CAPA_BAT_UPPER, CAPA_ELEC_UPPER, # CAPA_BAT_UPPER, CAPA_ELEC_UPPER
+    EBAT, FBAT, EELEC, # EBAT, FBAT, EELEC
+    COST_ELEC, COST_BAT, COST_TANK, # COST_ELEC, COST_BAT, COST_TANK
+    COST_WIND, COST_SOLAR, # COST_WIND, COST_SOLAR
+    0., # No initial charge
+    0., # No initial stock
+    missing, # No final charge constraint
+    missing, # No final stock constraint
+);
+
+print_solution_propreties(output2)
+
+prod2, cons2, bat2, tank2 = plot_solution(output2);
+
+prod2
+
+cons2
+
+bat2
+
+tank2
+
+# # Third Step : 
+# ## Solve the problem given the demand only.
+
+D = 1000.
+time_index = wind_profiles[YEAR]
+wind_profile = wind_profiles[YEAR]
+solar_profile = solar_profiles[YEAR];
+
+output3 = solve(
+    wind_profile, solar_profile, # WIND_PROFILE, SOLAR_PROFILE
+    DEMAND, # DEMAND
+    missing, missing, # WIND_CAPA, SOLAR_CAPA, if set to missing, the solver will optimize the capacities
+    missing, missing, missing, # BAT_SIZE, TANK_SIZE, ELEC_CAPA, if set to missing, the solver will optimize the capacities
+    PRICE_GRID, PRICE_CURTAILING, PRICE_PENALITY, # PRICE_GRID, PRICE_CURTAILING, PRICE_PENALITY
+    CAPA_BAT_UPPER, CAPA_ELEC_UPPER, # CAPA_BAT_UPPER, CAPA_ELEC_UPPER
+    EBAT, FBAT, EELEC, # EBAT, FBAT, EELEC
+    COST_ELEC, COST_BAT, COST_TANK, # COST_ELEC, COST_BAT, COST_TANK
+    COST_WIND, COST_SOLAR, # COST_WIND, COST_SOLAR
+    0., # No initial charge
+    0., # No initial stock
+    missing, # No final charge constraint
+    missing, # No final stock constraint
+);
+
+print_solution_propreties(output3)
+
+prod3, cons3, bat3, tank3 = plot_solution(output3);
+
+prod3
+
+cons3
+
+bat3
+
+tank3
---
+++ src/solver.jl
@@ -0,0 +1,203 @@
+using JuMP, Gurobi, HiGHS
+include("default_values.jl")
+
+
+"""
+## Arguments
+- wind_profile : Wind profile (% of the capacity)
+- solar_profile : Solar profile (% of the capacity)
+- wind_capa : Capacity of the wind farm (MW), if missing, is considered a variable to optimize
+- solar_capa : Capacity of the solar farm (MW), if missing, is considered a variable to optimize
+- battery_capa : Capacity of the battery (MWh), if missing, is considered a variable to optimize
+- tank_capa : Capacity of the tank (Kg), if missing, is considered a variable to optimize
+- elec_capa : Capacity of the electrolyzer (MW), if missing, is considered a variable to optimize
+- demand : Demand of hydrogen (Kg)
+- price_grid : Price of the grid (€ / MWh)
+- price_curtailing : Price of the curtailed energy (€ / MWh)
+- prodChangePenality : Penality for changing the production (€ / times the production changes)
+- ebat : Efficiency of the battery, discharge per month
+- fbat : Maximum flow of the battery (MW)
+- eelec : Efficiency of the electrolyzer (MWh / Kg)
+- cost_elec : Cost of the electrolyzer (€ / MW)
+- cost_bat : Cost of the battery (€ / MWh)
+- cost_tank : Cost of the tank (€ / Kg)
+- cost_wind : Cost of installing a wind turbine (€ / MW)
+- cost_solar : Cost of installing a solar panel (€ / MW)
+- initCharge : Initial charge of the battery (MWh)
+- initStock : Initial stock of the tank (Kg)
+- finalStock : Final stock of the tank (Kg), if None, the final stock is not constrained
+- finalCharge : Final charge of the battery (MWh), if None, the final charge is not constrained
+## Returns a dictionnary with following keys
+- wind_capa : Capacity of the wind farm (MW)
+- solar_capa : Capacity of the solar farm (MW)
+- battery_capa : Capacity of the battery (MWh)
+- tank_capa : Capacity of the tank (Kg)
+- electro_capa : Capacity of the electrolyzer (MW)
+- charge : Battery charge (MWh)
+- prod : Production of hydrogen (Kg)
+- stock : Stock of hydrogen in the tank (Kg)
+- elecGrid : Electricity consumption from the grid (MWh)
+- consPPA : Consumption of PPA energy (MWh)
+- flowBat : Flow of energy to / from the battery ( > 0 if charging, < 0 if discharging) (MWh)
+- flowH2 : Flow of hydrogen to / from the tank ( > 0 if charging, < 0 if discharging) (Kg)
+- operating_cost : Operating cost (€)
+- storage_cost : Construction cost of the storage (€)
+- electrolyser_cost : Construction cost of the electrolyzer (€)
+- electricity_plant_cost : Construction cost of the electricity production (€)
+"""
+function solve(
+    windProfile :: Array{Float64, 1},
+    solarProfile :: Array{Float64, 1},
+    demand :: Float64,
+    windCapa = missing,
+    solarCapa = missing,
+    batteryCapa  = missing,
+    tankCapa = missing,
+    electroCapa = missing,
+    price_grid = PRICE_GRID,
+    price_curtailing = PRICE_CURTAILING,
+    price_penality = PRICE_PENALITY,
+    capa_bat_upper = CELEC, # Upper bound for the electrolyzer capacity
+    capa_elec_upper = CAPA_BAT_UPPER, # Upper bound for the battery capacity
+    ebat = EBAT,
+    fbat = FBAT,
+    eelec = EELEC,
+    cost_elec = COST_ELEC,
+    cost_bat = COST_BAT,
+    cost_tank = COST_TANK,
+    cost_wind = PRICE_WIND,
+    cost_solar = PRICE_SOLAR,
+    initCharge =  0.,
+    initStock = 0.,
+    finalCharge = missing,
+    finalStock = missing
+)
+    # Number of time steps
+    T = length(windProfile)
+    if length(solarProfile) != T
+        throw(ArgumentError("The length of the solar profile should be equal to the length of the wind profile"))
+    end
+
+    # Create the model
+    model = Model(Gurobi.Optimizer)
+    println("Adding variables ... ")
+    # Electricity production capacities
+    if ismissing(windCapa)
+        windCapa = @variable(model, lower_bound = 0.)
+    end
+    if ismissing(solarCapa)
+        solarCapa = @variable(model, lower_bound = 0.)
+    end
+    # Storage variables
+    if ismissing(batteryCapa)
+        batteryCapa = @variable(model, lower_bound = 0., upper_bound = capa_bat_upper)
+    end
+    if ismissing(tankCapa)
+        tankCapa = @variable(model, lower_bound = 0.)
+    end
+    # Electrolyser capacity
+    if ismissing(electroCapa)
+        electroCapa = @variable(model, lower_bound = 0., upper_bound = capa_elec_upper)
+    end
+    # Main variables
+    charge = @variable(model, [1:T+1], lower_bound = 0.)
+    stock = @variable(model, [1:T+1], lower_bound = 0.)
+    prod = @variable(model, [1:T], lower_bound = 0.)
+    # Electricity consumption
+    elecPPA = @variable(model, [1:T], lower_bound = 0.)
+    elecGrid = @variable(model, [1:T], lower_bound = 0.)
+    curtailing = @variable(model, [1:T], lower_bound = 0.)
+    # Flow of elecricity / hydrogen
+    flowBat = @variable(model, [1:T])
+    flowH2 = @variable(model, [1:T])
+    # Production changing penality
+    if price_penality > 0
+        prodHasChanged = @variable(model, [2:T], Bin)
+    end
+    # Costs pseudo-variables
+    operating_cost = @variable(model)
+    storage_cost = @variable(model)
+    electrolyser_cost = @variable(model)
+    electricity_plant_cost = @variable(model)
+
+    println("Adding constraints ... ")
+    # Initial charge & stock
+    @constraint(model, charge[1] == initCharge)
+    @constraint(model, stock[1] == initStock)
+    # Final charge & stock
+    if !ismissing(finalCharge)
+        @constraint(model, charge[T+1] == finalCharge)
+    end
+    if !ismissing(finalStock)
+        @constraint(model, stock[T+1] == finalStock)
+    end
+    # Get the per hour discharge of the batteryn from the per month parameter
+    perHourDischarge = ebat ^ (1 / (30 * 24))
+    # PPA contract
+    @constraint(model, [t ∈ 1:T], elecPPA[t] == windProfile[t] * windCapa + solarProfile[t] * solarCapa)
+    # Demand satisfaction
+    @constraint(model, [t ∈ 1:T], prod[t] == flowH2[t] + demand)
+    # Electricity consumption
+    @constraint(model, [t ∈ 1:T], elecGrid[t] + elecPPA[t] - curtailing[t] == prod[t] * EELEC + flowBat[t])
+    # Battery charge
+    @constraint(model, [t ∈ 1:T], charge[t+1] == perHourDischarge * charge[t] + flowBat[t])
+    # Tank stock
+    @constraint(model, [t ∈ 1:T], stock[t+1] == 1 * stock[t] + flowH2[t])
+    # Flow of electricity / hydrogen
+    @constraint(model, [t ∈ 1:T], -fbat <= flowBat[t] <= fbat)
+    # Electrolyzer consumption
+    @constraint(model, [t ∈ 1:T], prod[t] * eelec <= electroCapa)
+    # Maximum charge & stock
+    @constraint(model, [t ∈ 1:T+1], charge[t] <= batteryCapa)
+    @constraint(model, [t ∈ 1:T+1], stock[t] <= tankCapa)
+    # Boolean variable for production change
+    if price_penality > 0
+        @constraint(model, [t ∈ 2:T], prod[t] - prod[t-1] <= 2 * capa_elec_upper * prodHasChanged[t] / eelec)
+        @constraint(model, [t ∈ 2:T], prod[t-1] - prod[t] <= 2 * capa_elec_upper * prodHasChanged[t] / eelec)
+    end
+    # Operating cost
+    # Operating cost
+    if price_penality > 0
+        @constraint(model, operating_cost == 
+        sum(elecGrid) * price_grid
+        + sum(curtailing) * price_curtailing 
+        + sum(prodHasChanged) * price_penality
+        )
+    else
+        @constraint(model, operating_cost == 
+        sum(elecGrid) * price_grid
+        + sum(curtailing) * price_curtailing
+        )
+    end
+    # Storage cost
+    @constraint(model, storage_cost == cost_bat * batteryCapa + cost_tank * tankCapa)
+    # Electrolyser cost
+    @constraint(model, electrolyser_cost == cost_elec * electroCapa)
+    # Electricity production cost
+    @constraint(model, electricity_plant_cost == windCapa * cost_wind + solarCapa * cost_solar)
+    # Objective
+    @objective(model, Min, operating_cost + storage_cost + electrolyser_cost + electricity_plant_cost)
+
+    println("Optimizing ...")
+    optimize!(model)
+
+    return Dict(
+        "wind_capa" => value.(windCapa),
+        "solar_capa" => value.(solarCapa),
+        "battery_capa" => value.(batteryCapa),
+        "tank_capa" => value.(tankCapa),
+        "electro_capa" => value.(electroCapa),
+        "charge" => value.(charge),
+        "prod" => value.(prod),
+        "stock" => value.(stock),
+        "elecGrid" => value.(elecGrid),
+        "curtail" => value.(curtailing),
+        "elecPPA" => value.(elecPPA),
+        "flowBat" => value.(flowBat),
+        "flowH2" => value.(flowH2),
+        "operating_cost" => value(operating_cost),
+        "storage_cost" => value(storage_cost),
+        "electrolyser_cost" => value(electrolyser_cost),
+        "electricity_plant_cost" => value(electricity_plant_cost)
+    )
+end
