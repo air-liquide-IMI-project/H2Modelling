28e9a432c3f937e30e0610ac48957a328cd07c63
Nicolas-Bessin <nicolasbessin49@gmail.com>
2024-04-09 17:06:33 +0200

    20 ev points, day by day generation

--- src/profile_generator.jl
+++ src/profile_generator.jl
@@ -27,9 +27,10 @@ function basic_possible_indexes(
     return possible_indexes
 end
 """
-    returns a list k of possible wind profiles for week t, assumed to be equiprobable
+    returns a list k of possible profiles for week t, assumed to be equiprobable
+    The returned profiles are whole weeks from the training set
 """
-function basic_wind_generator(
+function generate_from_weeks(
     t :: Int,
     wind_train :: Array{Array{Float64, 1}},
     k :: Int = 1,
@@ -43,16 +44,37 @@ function basic_wind_generator(
 end
 
 """
-    returns a list k of possible solar profiles for week t, assumed to be equiprobable
+    returns a list k of possible load profiles for week t, assumed to be equiprobable.
+    To generate the profiles, we build a new profile by recomposing days from the training set
+    We have to be careful, because the training set is already divided into weeks.
 """
-function basic_solar_generator(
-    t :: Int,
-    solar_train :: Array{Array{Float64, 1}},
+function generate_from_days(
+    t :: Int, # week index
+    wind_train :: Array{Array{Float64, 1}},
     k :: Int = 1,
     around :: Int = 3
 )
-    possible_indexes = basic_possible_indexes(t, length(solar_train))
-    chosen_indexes = sample(possible_indexes, k, replace = false)
-    return solar_train[chosen_indexes]
-
+    n_train = length(wind_train)
+    # First choose from which weeks we can take the data
+    week_index = basic_possible_indexes(t, n_train, around)
+    generated_wind = Array{Array{Float64, 1}}(undef, k)
+    for i in 1:k
+        # For each week, we choos 7 days at random from the training set
+        for j in 1:7
+            if j == 1
+                generated_wind[i] = []
+            end
+            chosen_week = sample(week_index, 1)[1]
+            chosen_day = rand(1:7)
+            # Get the beginning of the day in the hourly data
+            index_begin = (chosen_day-1)*24 + 1
+            index_end = chosen_day*24
+            day = wind_train[chosen_week][index_begin:index_end]
+            generated_wind[i] = vcat(generated_wind[i], day)
+        end
+    end
+    
+    return generated_wind
 end
+
+
