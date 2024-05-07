fbfab54d4286ce948997b06f9dfd6a428d48ffb2
Nicolas-Bessin <nicolasbessin49@gmail.com>
2024-05-07 14:06:55 +0200

    unifrom thresholds classification

--- src/classifier.jl
+++ src/classifier.jl
@@ -1,18 +1,21 @@
 include("./constants.jl")
 
 """
-Compute the classes using the given list of profiles, based on total energy produced
-Returns the threshold values for each class, and the associated probabilities (counting the number of profiles in each class)
+Compute the classes using the given list of profiles (over the training profiles), based on total energy produced
+Returns the classes for each profile and the corresponding probas (by counting the number of profiles in each class in the training set)
 """
-function compute_classes(
-    wind_by_periods :: Vector{Vector{Float64}},
-    solar_by_periods :: Vector{Vector{Float64}},
+function classify_uniform_thresholds(
+    wind_by_periods_train :: Vector{Vector{Float64}},
+    solar_by_periods_train :: Vector{Vector{Float64}},
+    wind_by_periods_val :: Vector{Vector{Float64}},
+    solar_by_periods_val :: Vector{Vector{Float64}},
     k::Int
     )
+    ## First step, compute the thresholds
     # Compute the total energy produced for each period
-    n = length(wind_by_periods)
+    n = length(wind_by_periods_train)
     total_energy = [
-        sum(wind_by_periods[i]) * WIND_CAPA + sum(solar_by_periods[i]) * SOLAR_CAPA
+        sum(wind_by_periods_train[i]) * WIND_CAPA + sum(solar_by_periods_train[i]) * SOLAR_CAPA
         for i in 1:n
     ]
     # Divide the data into k classes uniformly spaced between the min and max values
@@ -21,48 +24,37 @@ function compute_classes(
     thresholds = range(min_energy, max_energy, length=k+1)
     # Exclude the first and last thresholds (because they are the min and max values)
     true_thresholds = [ thresholds[i] for i in 2:k ]
-    # Count the number of profiles in each class
+    # Count the number of profiles in each class, and compute the training classes
     counts = zeros(Int, k)
+    classes_train = zeros(Int, n)
     for i in 1:n
         class = 1
         while class < k && total_energy[i] > true_thresholds[class] 
             class += 1
         end
         counts[class] += 1
+        classes_train[i] = class
     end
     # Compute the probabilities
     probs = counts ./ n
-    println("Thresholds : ", vec(true_thresholds))
-    println("Minimum energy : ", min_energy, " Maximum energy : ", max_energy)
-    return true_thresholds, probs
-end
-
-"""
-Classify the profiles into classes based on the total energy produced,
-using the given thresholds (computed by compute_classes)
-Returns the class index for each profile
-"""
-function classify_profiles(wind_by_periods, solar_by_periods, thresholds)
-    n = length(wind_by_periods)
-    k = length(thresholds) + 1
-    if n != length(solar_by_periods)
-        throw(ArgumentError("The number of wind and solar profiles must be the same"))
-    end
+    
+    ## Second step, classify the validation profiles
     # Compute the total energy produced for each period
-    total_energy = [
-        sum(wind_by_periods[i]) * WIND_CAPA + sum(solar_by_periods[i]) * SOLAR_CAPA
-        for i in 1:n
+
+    n_val = length(wind_by_periods_val)
+    total_energy_val = [
+        sum(wind_by_periods_val[i]) * WIND_CAPA + sum(solar_by_periods_val[i]) * SOLAR_CAPA
+        for i in 1:n_val
     ]
-    # Classify the profiles
-    classes = zeros(Int, n)
-    for i in 1:n
-        # Compute the class index = the first threshold strictly below the total energy produced
+    # Count the number of profiles in each class, and compute the validation classes
+    classes_val = zeros(Int, n_val)
+    for i in 1:n_val
         class = 1
-        while class < k && total_energy[i] > thresholds[class]
+        while class < k && total_energy_val[i] > true_thresholds[class] 
             class += 1
         end
-        classes[i] = class
+        classes_val[i] = class
     end
-    return classes
+
+    return probs, classes_train, classes_val
 end
-    
